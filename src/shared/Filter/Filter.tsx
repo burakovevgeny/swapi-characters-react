252afd89aa4e2b2ea6bdb65648196dc707f896d0
burakovevgeny <imzhwch@gmail.com>
2021-11-18 23:55:51 +0700

    fix click to title

--- src/shared/Filter/Filter.tsx
+++ src/shared/Filter/Filter.tsx
@@ -29,16 +29,16 @@ const Filter: FC<PropsTypes> = ({ data, setFilteredPeople }) => {
     <S.FilterWrapper>
       <S.Input type='text' value={search} onChange={onSearchValue} />
       <S.GenderWrapper>
-        <S.GenderTitle active={filter === Gender.FEMALE}>
-          <Icon icon='mdi-gender-female' onClick={() => setFilter(Gender.FEMALE)} />
+        <S.GenderTitle active={filter === Gender.FEMALE} onClick={() => setFilter(Gender.FEMALE)}>
+          <Icon icon='mdi-gender-female' />
           <p>Female</p>
         </S.GenderTitle>
-        <S.GenderTitle active={filter === Gender.MALE}>
-          <Icon icon='mdi-gender-male' onClick={() => setFilter(Gender.MALE)} />
+        <S.GenderTitle active={filter === Gender.MALE} onClick={() => setFilter(Gender.MALE)}>
+          <Icon icon='mdi-gender-male'/>
           <p>Male</p>
         </S.GenderTitle>
-        <S.GenderTitle active={filter === Gender.ALL}>
-          <Icon icon='mdi-gender-male-female' onClick={() => setFilter(Gender.ALL)} />
+        <S.GenderTitle active={filter === Gender.ALL} onClick={() => setFilter(Gender.ALL)}>
+          <Icon icon='mdi-gender-male-female'/>
           <p>All</p>
         </S.GenderTitle>
       </S.GenderWrapper>
